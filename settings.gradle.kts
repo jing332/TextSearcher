2c98bfb2c0a2391204a797203b1e264606f9e1a0
Jing <42014615+jing332@users.noreply.github.com>
2023-07-16 16:13:46 +0800

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+        maven("https://jitpack.io")
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+}
+
+rootProject.name = "txtSearcher"
+include(":app")
